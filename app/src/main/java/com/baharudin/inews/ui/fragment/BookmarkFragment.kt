52776ca3f6140e9203d767998b9505290b8a4217
Exsan Renaldhi <exsan172@gmail.com>
2021-11-04 22:19:48 +0700

    itemTouchHelper

--- app/src/main/java/com/baharudin/inews/ui/fragment/BookmarkFragment.kt
+++ app/src/main/java/com/baharudin/inews/ui/fragment/BookmarkFragment.kt
@@ -6,11 +6,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.baharudin.inews.R
 import com.baharudin.inews.adapter.RekomendasiAdapter
 import com.baharudin.inews.databinding.FragmentBookmarkBinding
 import com.baharudin.inews.ui.viewmodel.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -29,6 +32,35 @@ class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
 
         setupRecycleview()
         subcribeNews()
+
+        val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = adapterNews.newsList[position]
+                newsViewModel.deleteBookmark(article)
+                Snackbar.make(view, "Succesfully delete Bookmark", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        newsViewModel.saveBookmark(article)
+                    }
+                    show()
+                }
+            }
+
+        }
+        ItemTouchHelper(itemTouchCallback).apply {
+            attachToRecyclerView(binding.rvBookamrk)
+        }
 
     }
     private fun setupRecycleview() {
