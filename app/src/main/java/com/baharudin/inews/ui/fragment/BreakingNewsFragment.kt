211ad8375352ff94c72ffdb1f0bcf64a16e6bbd8
Exsan Renaldhi <exsan172@gmail.com>
2021-11-08 20:08:14 +0700

    new fragment

---
+++ app/src/main/java/com/baharudin/inews/ui/fragment/BreakingNewsFragment.kt
@@ -0,0 +1,15 @@
+package com.baharudin.inews.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class BreakingNewsFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/baharudin/inews/ui/fragment/RekomendasiNewsFragment.kt
@@ -0,0 +1,9 @@
+package com.baharudin.inews.ui.fragment
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class RekomendasiNewsFragment : Fragment() {
+}
